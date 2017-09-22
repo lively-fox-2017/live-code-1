b9ff8dc2efa5924668ef9dae64cd461d758a6563
aditReza <lete.note3@gmail.com>
2017-09-22 11:01:11 +0700

    xoxoxo

--- coinCombination/coin.js
+++ coinCombination/coin.js
@@ -1,8 +1,17 @@
 function getCoins(value) {
-	//write your code here
+    //write your code here
+    let coin = [1, 5, 7, 9, 11]
+    let revCoin = coin.sort(function(a, b) { return b - a })
+    let result = []
+    console.log(coin)
+
+    for (let i = 0; i < coin.length; i++) {
+
+    }
+
 }
 
 
 getCoins(12) //[1, 11] dan [5, 7]
-getCoins(17) //[1, 5, 11] dan [1,7,9]
-getCoins(3) //-1
+    // getCoins(17) //[1, 5, 11] dan [1,7,9]
+    // getCoins(3) //-1
