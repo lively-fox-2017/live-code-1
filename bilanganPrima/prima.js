978db05c67b860a86c8f257305bdce27a2bdd812
Yakopoto <mfajarpamungkas@icloud.com>
2017-09-22 11:11:15 +0700

    push

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,13 +1,28 @@
+"use strict"
+
 function isPrime(angka) {
   //your code here
+  if(angka === 1){
+    return false;
+  }
+  
+  for(let i = 1; i < angka ; i++){
+    if(i !== 1 && i !== angka && angka % i === 0){
+      return false;
+    }
+  }
+
+  return true;
 }
 
 function getPrime(deret) {
   //your code here
+  
+
 }
 
-isPrime(1) //false
-isPrime(5) //true
+console.log(isPrime(1)) //false
+console.log(isPrime(5)) //true
 
 getPrime(1) //2
 getPrime(5) //11
