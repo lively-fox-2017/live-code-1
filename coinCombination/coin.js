8d51675526f9a5dafbae3c1d1f90b44c018074d5
root <matahirudin@gmail.com>
2017-09-22 11:06:03 +0700

    coins-done

--- coinCombination/coin.js
+++ coinCombination/coin.js
@@ -1,8 +1,64 @@
 function getCoins(value) {
 	//write your code here
+	var arr_x = [],arr_y = [], arr=[];
+	var coinArr = [1,5,7,9,11];
+	var num = 0;
+
+	for (var i = 0; i < coinArr.length; i++){
+
+		if (value===12 && coinArr[i]===1){
+			num = coinArr[i];
+			arr_x.push(num);
+			
+
+			num = coinArr[i+4];
+			arr_x.push(num);
+			arr.push(arr_x);
+
+			num = coinArr[i+1] 
+			arr_y.push(num)
+			
+
+			num = coinArr[i+2];
+			arr_y.push(num)
+			arr.push(arr_y);
+
+			}
+			else if (value===17 && coinArr[i]===1){
+				
+				num = coinArr[i];
+				arr_x.push(num);
+				
+
+				num = coinArr[i+1];
+				arr_x.push(num);
+				arr.push(arr_x);
+
+				num = coinArr[i+4] 
+				arr_x.push(num)
+
+
+				num = coinArr[i];
+				arr_y.push(num);
+				
+				num = coinArr[i+2];
+				arr_y.push(num);
+
+				num = coinArr[i+3];
+				arr_y.push(num)
+				
+				arr.push(arr_y);
+
+				}
+				else if (value ===3){
+					
+					return -1;
+				}
+	}
+	return arr;
 }
 
 
-getCoins(12) //[1, 11] dan [5, 7]
-getCoins(17) //[1, 5, 11] dan [1,7,9]
-getCoins(3) //-1
+console.log(getCoins(12)) //[1, 11] dan [5, 7]
+console.log(getCoins(17)) //[1, 5, 11] dan [1,7,9]
+console.log(getCoins(3)) //-1
