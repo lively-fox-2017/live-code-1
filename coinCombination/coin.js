504ad8907cc59f01eb514bf8ca5fca5de683d790
nach9 <nur.achdiansyah@gmail.com>
2017-09-22 11:01:05 +0700

    not finish

--- coinCombination/coin.js
+++ coinCombination/coin.js
@@ -1,5 +1,18 @@
 function getCoins(value) {
 	//write your code here
+  var coins = [1,5,7,9,11]
+  var arr=[]
+  var cek=0
+
+  for (var i=coins.length-1 ; i>=0 ;i--){
+    cek = value - (coins[i])
+
+  }
+
+  if (arr.length==0){
+    console.log(-1);
+  }
+
 }
 
 
