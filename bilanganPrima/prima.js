ddf61bc7fc03e110887b753e84e2cace8ecc008f
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-22 09:44:13 +0700

    Bilangan Prima Release 1

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,14 +1,37 @@
 function isPrime(angka) {
-  //your code here
+  let test = 2;
+  if (angka === 2)
+    return true;
+  if (angka === 1)
+    return false;
+  let rootAngka = Math.sqrt(angka);
+  while (test <= rootAngka) {
+    if (angka % test === 0)
+      return false;
+    test++;
+  }
+  return true;
 }
 
 function getPrime(deret) {
-  //your code here
+  let i = 1;
+  let prime = 1;
+  let result;
+  while (i <= deret) {
+    if (isPrime(prime)) {
+      i++;
+      result = prime
+    }
+    prime++;
+  }
+  return result;
 }
 
-isPrime(1) //false
 isPrime(5) //true
+isPrime(1) //false
+console.log(isPrime(6)); //false
 
 getPrime(1) //2
 getPrime(5) //11
-getPrime(11) //19
+getPrime(8) //19
+getPrime(11) //31
