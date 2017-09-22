3884601a566a8e0471a54e8b96806fbf4f14e49c
terrathe2 <putra.redha@gmail.com>
2017-09-22 14:07:44 +0700

    benerin *sementara

--- shapeOfW/shapeofw.js
+++ shapeOfW/shapeofw.js
@@ -2,13 +2,29 @@ function reorderingLetter(str, totalLevel) {
   //your code here...
   let arrStr = str.split("");
   let hasil = [];
-  let banding = Math.floor(arrStr.length/totalLevel);
   
   for(let i=0;i<totalLevel;i++){
 	hasil.push([]);
   }
   
-  
+  let j = 0;
+  let temp = true;
+  while(arrStr.length > 0){
+	hasil[j].push(arrStr[0]);
+	arrStr.shift();
+	
+	if(temp){
+		j++;
+	}else{
+		j--;
+	}
+	
+	if(j === totalLevel-1){
+		temp = false;
+	}else if(j === 0){
+		temp = true;
+	}
+  }
   
   console.log(hasil);
 }
