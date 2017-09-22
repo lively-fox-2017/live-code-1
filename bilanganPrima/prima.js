5b9f3587723be51e43cd56a32932714e8a0a451d
Dimitri <deathmitri@gmail.com>
2017-09-22 10:26:31 +0700

    Finished bilanganPrima, Release 1 and 2
    Added some comments

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,37 +1,61 @@
-let primes = [];
-
 function isPrime(angka) {
 
   let counter = 0;
 
   if (angka === 1) {
 
-    return console.log(false);;
+    return false;
 
   }
 
+  // Loop from 2 to angka
   for (let i = 2; i < angka; i++) {
 
+    // Straight return false if `angka` % `i` === 0 (not PRIME)
     if (angka % i === 0) {
 
-      return console.log(false);
+      return false;
 
     }
 
   }
 
-  return console.log(true);
+  return true;
 
 }
 
-function getPrime(deret) {
-  //your code here
+function getPrime(deret, primes = [], currentPrime = 1) {
+
+  if (primes.length === deret) {
+
+    return currentPrime;
+
+  }
+
+  // If exists, don't increment by 1
+  if (primes.indexOf(currentPrime) !== -1) {
+
+    currentPrime += 1;
+
+  }
+
+  // Find the prime
+  while (!isPrime(currentPrime)) {
+
+    currentPrime += 1;
+
+  }
+
+  // Push the prime
+  primes.push(currentPrime);
+
+  return getPrime(deret, primes, currentPrime);
+
 }
 
 isPrime(1) //false
 isPrime(5) //true
 isPrime(22) //false
 
-// getPrime(1) //2
-// getPrime(5) //11
-// getPrime(11) //19
+getPrime(8);// 19
+getPrime(11);; //31
