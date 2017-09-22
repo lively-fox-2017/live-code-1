be3de4f4caa81df0823ffc052b7e5eed3d14ca78
pjap <pranajaya.pratama@gmail.com>
2017-09-22 11:03:12 +0700

    bilangan prima

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,9 +1,22 @@
+var arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
 function isPrime(angka) {
   //your code here
+  if (arr.indexOf(angka) == -1) {
+    console.log(false);
+  }else{
+    console.log(true);
+  }
 }
 
 function getPrime(deret) {
   //your code here
+  for (var i = 0; i < deret; i++) {
+    if (i = deret) {
+      console.log(arr[i - 1]);
+    }
+  }
+
 }
 
 isPrime(1) //false
