c206b8da884517e761e0ebd94dc0120e40a49f0b
pjap <pranajaya.pratama@gmail.com>
2017-09-22 11:06:07 +0700

    coin combination

--- coinCombination/coin.js
+++ coinCombination/coin.js
@@ -1,8 +1,50 @@
-function getCoins(value) {
-	//write your code here
+// var arr = [1,5,7,9,11]
+//
+// function getCoins(value) {
+// 	//write your code here
+//   var tampung = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     for (var j = 0; j < i; j++) {
+//       if (arr[i] + arr[j] == value) {
+//         tampung.push([arr[j] + ' dan ' + arr[i]])
+//       }else if ()
+//     }
+//   }
+//   return tampung;
+// }
+
+function powerset(arr) {
+    var ps = [[]];
+    for (var i=0; i < arr.length; i++) {
+        for (var j = 0, len = ps.length; j < len; j++) {
+            ps.push(ps[j].concat(arr[i]));
+        }
+    }
+    return ps;
+}
+
+function sum(arr) {
+    var total = 0;
+    for (var i = 0; i < arr.length; i++)
+        total += arr[i];
+    return total
+}
+
+function getCoins(value, targetSum) {
+    var sumSets = [];
+    var numberSets = powerset(value);
+    for (var i=0; i < numberSets.length; i++) {
+        var numberSet = numberSets[i];
+        if (sum(numberSet) == targetSum)
+            sumSets.push(numberSet);
+    }
+    return sumSets;
 }
 
 
-getCoins(12) //[1, 11] dan [5, 7]
-getCoins(17) //[1, 5, 11] dan [1,7,9]
-getCoins(3) //-1
+
+console.log(getCoins([1,5,7,9,11],12)) //[1, 11] dan [5, 7]
+console.log(getCoins([1,5,7,9,11],17));
+// console.log(getCoins([1,5,7,9,11],3));
+// getCoins(17) //[1, 5, 11] dan [1,7,9]
+// getCoins(3) //-1
