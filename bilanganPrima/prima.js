3b93ab22cb975564407de8e4166701cf1a3ac5dd
albertagung <albiardtya@gmail.com>
2017-09-22 11:00:47 +0700

    albertagung

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,14 +1,31 @@
 function isPrime(angka) {
   //your code here
+  if(angka === 1){
+    return false
+  }
+  for(var y = angka-1; y>= 2; y--){
+    console.log(y);
+    if(angka % y === 0){
+      return false
+    }
+  }
+  return true
 }
 
 function getPrime(deret) {
   //your code here
+  var number = 5;
+  var tampung = [];
+  for(var j = number-1; j >= 2; j--){
+    if(number % j !== 0){
+      console.log(j);
+    }
+  }
 }
 
-isPrime(1) //false
-isPrime(5) //true
+console.log(isPrime(1)) //false
+console.log(isPrime(5)) //true
 
-getPrime(1) //2
-getPrime(5) //11
-getPrime(11) //19
+console.log(getPrime(1)) //2
+// getPrime(5) //11
+// getPrime(11) //19
--- coinCombination/coin.js
+++ coinCombination/coin.js
@@ -1,8 +1,25 @@
 function getCoins(value) {
 	//write your code here
+	var arr = [1,5,7,9,11]
+	var abc = [];
+	for(var i = 0; i < arr.length; i++){
+		console.log(arr[i]);
+		arrI = arr[i];
+		for(var y = 0; y < arr.length; y++){
+			console.log(arr[y]);
+			var count = arr[i] + arr[y];
+			console.log(count)
+			if(count === value){
+				console.log(arr[i]);
+				abc.push(arr[i])
+			}
+		}
+	}
+	console.log(abc.sort());
+	return(abc.sort());
 }
 
 
-getCoins(12) //[1, 11] dan [5, 7]
-getCoins(17) //[1, 5, 11] dan [1,7,9]
-getCoins(3) //-1
+console.log(getCoins(12)) //[1, 11] dan [5, 7]
+// console.log(getCoins(17)) //[1, 5, 11] dan [1,7,9]
+// getCoins(3) //-1
