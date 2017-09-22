cc607f35417619d97c8236812a07ce23d8eed5d4
wisnujaya <jayalahwisnu@gmail.com>
2017-09-22 09:24:10 +0700

    release 0

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,14 +1,24 @@
 function isPrime(angka) {
   //your code here
+  if(angka == 1) {
+    return false
+  }
+  
+  for(var i = 2; i < angka; i++) {
+    if(angka % i == 0) {
+      return false
+    }
+    return true
+  }
 }
 
 function getPrime(deret) {
   //your code here
 }
 
-isPrime(1) //false
-isPrime(5) //true
+console.log(isPrime(1)) //false
+console.log(isPrime(5)) //true
 
-getPrime(1) //2
-getPrime(5) //11
-getPrime(11) //19
+//getPrime(1) //2
+//getPrime(5) //11
+//getPrime(11) //19
