38c2308bfdd4a382fd573ff29f02d394ab7a6834
munawirali <munawirali@gmail.com>
2017-09-22 10:44:39 +0700

    done

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,14 +1,31 @@
 function isPrime(angka) {
-  //your code here
+  var max = Math.sqrt(angka);
+  if (angka==1) {
+    return false
+  }
+  for( var i = 2;  i <= max;  i++ ) {
+      if( angka % i == 0 )
+          return false;
+  }
+  return true;
 }
 
 function getPrime(deret) {
   //your code here
+  prima=[];
+  for (var i = 2; i < 100; i++) {
+    if (isPrime(i)){
+      prima.push(i);
+    }
+  }
+  // console.log(prima);
+  return prima[deret-1]
 }
 
 isPrime(1) //false
 isPrime(5) //true
-
+console.log(isPrime(1));
 getPrime(1) //2
 getPrime(5) //11
-getPrime(8) //19
+getPrime(11) //19
+console.log(getPrime(5));
