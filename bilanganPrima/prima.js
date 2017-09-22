e6a80aa8776ca1f9fcc14604d3f25ae94fac31e3
aditReza <lete.note3@gmail.com>
2017-09-22 10:31:13 +0700

    release 2 done

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,14 +1,33 @@
 function isPrime(angka) {
-  //your code here
+    //your code here
+    let prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    let counter = 0
+    for (let i = 0; i < prime.length; i++) {
+        //console.log(prime[i])
+        if (angka === prime[i]) {
+            return true
+        }
+    }
+    return (counter > 0) ? true : false
 }
 
 function getPrime(deret) {
-  //your code here
+    //your code here
+    let prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    let result = []
+
+    result.push(prime[deret - 1])
+    return result.join();
 }
 
 isPrime(1) //false
 isPrime(5) //true
+console.log(isPrime(1));
+console.log(isPrime(5))
 
 getPrime(1) //2
 getPrime(5) //11
-getPrime(11) //19
+getPrime(8) //19
+console.log(getPrime(1))
+console.log(getPrime(5))
+console.log(getPrime(8))
