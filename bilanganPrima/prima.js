56542a5d0a05eb224cd564a405c2ea402bcdfa2c
kholes <cholis.elbazar@gmail.com>
2017-09-22 11:01:57 +0700

    hmmmm

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,14 +1,31 @@
 function isPrime(angka) {
   //your code here
+    var prime = angka != 1;
+    for(var i=2; i<angka; i++) {
+        if(angka % i == 0) {
+            prime = false;
+            break;
+        }
+    }
+    return prime;
 }
 
 function getPrime(deret) {
   //your code here
+  if(deret == 1){
+  	return 2
+  }else{
+  	let c=0
+  	if(isPrime(deret)==true){
+  		c+=deret
+  	}
+	return getPrime(deret-1)
+  }
 }
 
-isPrime(1) //false
-isPrime(5) //true
+console.log(isPrime(1)) //false
+console.log(isPrime(5)) //true
 
-getPrime(1) //2
-getPrime(5) //11
-getPrime(11) //19
+// console.log(getPrime(1)) //2
+console.log(getPrime(5)) //11
+getPrime(8) //19
--- shapeOfW/shapeofw.js
+++ shapeOfW/shapeofw.js
@@ -1,6 +1,18 @@
 function reorderingLetter(str, totalLevel) {
   //your code here...
+  	let text='';
+  	let count=0
+  	let txt=0
+  	for(let i=0;i<totalLevel;i++){
+  		while(count < totalLevel){
+  			text+=str[txt]
+  			count ++
+  			txt+=4
+  		}
+  		count+=txt
+  	}
+  	console.log(text)
 }
 
-reorderingLetter(“DARWINFOX”, 3) //DIXAWNORF
-reorderingLetter(“ABCDEFGHIJKLMNOPQ”, 5) //AIQBHJPCGKODFLNEM
+reorderingLetter("DARWINFOX", 3) //DIXAWNORF
+// reorderingLetter(“ABCDEFGHIJKLMNOPQ”, 5) //AIQBHJPCGKODFLNEM
