fd9e9257688059fa965dafd2b096337f7be0dd4f
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-22 09:38:13 +0700

    bilangan prima selesai

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,14 +1,34 @@
 function isPrime(angka) {
   //your code here
+  let guess = 0
+  for (let i = 1; i <= angka; i++) {
+    if (angka%i === 0) {
+      guess += i
+    }
+  }
+  if (angka+1 === guess) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function getPrime(deret) {
   //your code here
+  let count = 0
+  let deretPrima = 0
+  while (count < deret) {
+    deretPrima++
+    if(isPrime(deretPrima)){
+      count++
+    }
+  }
+  return deretPrima
 }
 
-isPrime(1) //false
-isPrime(5) //true
+// console.log(isPrime(1)) //false
+// console.log(isPrime(5)) //true
 
-getPrime(1) //2
-getPrime(5) //11
-getPrime(11) //19
+console.log(getPrime(1)) //2
+console.log(getPrime(5)) //11
+console.log(getPrime(8)) //19
