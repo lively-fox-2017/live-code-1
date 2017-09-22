21ac5555e82b0a0c556bc05a6a79d556b93cfd22
pjap <pranajaya.pratama@gmail.com>
2017-09-22 11:07:36 +0700

    shapeofW

--- shapeOfW/shapeofw.js
+++ shapeOfW/shapeofw.js
@@ -1,6 +1,18 @@
 function reorderingLetter(str, totalLevel) {
   //your code here...
+  // var row = 0;
+  // var temp = []
+  // for (var i = 0; i < str.length; i++) {
+  //     temp.push([str[i]])
+  // }
+  // return temp
+  var tampung = [];
+
+  for (var i = 0; i < str.length; i+=totalLevel+1) {
+      tampung.push(str[i])
+  }
+  return tampung
 }
 
-reorderingLetter(“DARWINFOX”, 3) //DIXAWNORF
-reorderingLetter(“ABCDEFGHIJKLMNOPQ”, 5) //AIQBHJPCGKODFLNEM
+console.log(reorderingLetter("DARWINFOX", 3)) //DIXAWNORF
+// reorderingLetter("ABCDEFGHIJKLMNOPQ", 5) //AIQBHJPCGKODFLNEM
