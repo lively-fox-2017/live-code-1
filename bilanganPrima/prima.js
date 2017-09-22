230a2157e1bc9ea2eddc8cfc029bdebfb07c2019
kalibani <kalibani.ka@gmail.com>
2017-09-22 11:02:58 +0700

    livecode bany

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,13 +1,25 @@
 function isPrime(angka) {
   //your code here
+  if(angka==1){
+    console.log(false)
+  }
+  for(i=2;i<Math.sqrt(angka);i++){
+    if(angka % i==0){
+    console.log(false);
+    }else {
+      console.log(true);
+    }
+  }
+}
+function getPrime() {
+
 }
 
-function getPrime(deret) {
-  //your code here
-}
 
-isPrime(1) //false
-isPrime(5) //true
+
+isPrime(1); //false
+isPrime(5);//true
+
 
 getPrime(1) //2
 getPrime(5) //11
--- coinCombination/coin.js
+++ coinCombination/coin.js
@@ -1,6 +1,23 @@
 function getCoins(value) {
 	//write your code here
+  var arr=[]
+  for (var i = 0; i < value; i++) {
+    arr.push(i)
+  }
+  for( var m = arr.length;  m; ) {
+      var i = Math.floor( Math.random() * m-- );
+      var t = arr[m];
+      arr[m] = arr[i];
+      arr[i] = t;
+  }
+  var output = [];
+  for( var i = 0, n = arr.length-1;  i < n;  i += 2 ) {
+      output.push([ arr[i], arr[i+1] ]);
+  }
+  console.log(output);
+
 }
+
 
 
 getCoins(12) //[1, 11] dan [5, 7]
