0f8ae5e7a58769292f52d4239a4c726ba47e05c5
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-22 11:00:55 +0700

    live code 1

--- coinCombination/coin.js
+++ coinCombination/coin.js
@@ -1,8 +1,47 @@
 function getCoins(value) {
 	//write your code here
-}
+	// Koin yang ada 1, 5, 7, 9, dan 11
+	let coin = [1, 5, 7, 9, 11];
+	let count = 0
+	let guess = []
+	let temp = 0
+	for (let i =0; i < coin.length; i++) {
+
+		// if (count + coin[i] <= value) (
+		// 	count += coin[i]
+		// )
+		// 	guess.push(coin[i])
+		// 	console.log(count)
+		// if (count < value) {
+		// 	count += coin[i]
+		// }
+		// coin = coin.slice(i)
+		// console.log(coin)
+		// value -= coin[i]
+		// // guess.push(coin[i])
+		// if (coin.indexOf(value)!== -1) {
+		// 	guess.push(coin[i])
+		// 	guess.push(coin[coin.indexOf(value)])
+		// }
+		if (count + coin[i] <= value) {
+			count += coin[i]
+			guess.push(coin[i])
+			console.log(count)
+	
+			// console.log(coin.indexOf(value))
+		}
+	}
+	let sum = guess.reduce((a,b)=>{return a+b})
+	if (sum > value){
+		guess.shift()
+	}
+
+	// console.log(count)
+// 	guess = guess.slice(0,guess.length/2)
+	console.log(guess)
+ }
 
 
 getCoins(12) //[1, 11] dan [5, 7]
-getCoins(17) //[1, 5, 11] dan [1,7,9]
-getCoins(3) //-1
+// getCoins(17) //[1, 5, 11] dan [1,7,9]
+// getCoins(3) //-1
--- shapeOfW/shapeofw.js
+++ shapeOfW/shapeofw.js
@@ -1,6 +1,25 @@
 function reorderingLetter(str, totalLevel) {
   //your code here...
+  let board = []
+  let k =0
+  // let 
+  for (let i = 0; i < totalLevel; i++){
+    board.push([])
+  }
+  console.log(board)
+  for (let i = 0; i < str.length; i++){
+    board[k].push(str[i])
+    if (k < totalLevel - 1) {
+      k++
+    }
+    if (k >= totalLevel) {
+      k--
+    } else {
+      k =0
+    }
+  }
+    console.log(board)
 }
 
-reorderingLetter(“DARWINFOX”, 3) //DIXAWNORF
-reorderingLetter(“ABCDEFGHIJKLMNOPQ”, 5) //AIQBHJPCGKODFLNEM
+reorderingLetter('DARWINFOX', 3) //DIXAWNORF
+// reorderingLetter('ABCDEFGHIJKLMNOPQ', 5) //AIQBHJPCGKODFLNEM
