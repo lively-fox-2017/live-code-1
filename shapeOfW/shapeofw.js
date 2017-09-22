6fac34624ddc0f0422e5dbf65c4d02890a3c41af
munawirali <munawirali@gmail.com>
2017-09-22 11:00:35 +0700

    done

--- shapeOfW/shapeofw.js
+++ shapeOfW/shapeofw.js
@@ -1,5 +1,14 @@
 function reorderingLetter(str, totalLevel) {
   //your code here...
+  let result=[];
+  for (var i = 0; i < totalLevel; i++) {
+    totalLevel.push([])
+  }
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < totalLevel; j++) {
+      result+=str[str.length%totalLevel]
+    }
+  }
 }
 
 reorderingLetter(“DARWINFOX”, 3) //DIXAWNORF
