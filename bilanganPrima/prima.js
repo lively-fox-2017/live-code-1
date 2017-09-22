d3ba3f85c780401e8e845a8cad54b41158e90405
nach9 <nur.achdiansyah@gmail.com>
2017-09-22 10:52:29 +0700

    done

--- bilanganPrima/prima.js
+++ bilanganPrima/prima.js
@@ -1,14 +1,30 @@
-function isPrime(angka) {
-  //your code here
-}
+
+'use strict'
+
+function isPrime(angka){
+    let arr=[]
+    for (let i = 1 ; i <= angka ;i++){
+      if (angka % i==0){
+        arr.push(i)
+      }
+    }
+    console.log( arr.length ==2)
+  }
+
+
 
 function getPrime(deret) {
   //your code here
+  if(deret==1){
+    console.log(2);
+  }
+
 }
+
 
 isPrime(1) //false
 isPrime(5) //true
 
 getPrime(1) //2
-getPrime(5) //11
-getPrime(11) //19
+// getPrime(5) //11
+// getPrime(11) //19
--- shapeOfW/shapeofw.js
+++ shapeOfW/shapeofw.js
@@ -1,6 +1,41 @@
 function reorderingLetter(str, totalLevel) {
   //your code here...
+  var arr =[]
+  var start = true //nentuin maju mundur
+  var level = -1
+  var word =''
+  //persiapan array
+  for (var i=0;i<totalLevel ; i++){
+    var arrRow = []
+    arr.push(arrRow)
+  }
+  //pemecahan huruf
+  for( var i=0 ; i<str.length ;i++){
+    if (start){
+      level++
+      if(level==totalLevel-1){
+        start=false
+      }
+
+    }
+    else{
+      level--
+      if(level==0){
+        start=true
+      }
+
+    }
+    arr[level].push(str[i])
+  }
+ //gabungin kata
+  for (var i=0;i<totalLevel ; i++){
+    word += arr[i].join('')
+  }
+
+
+
+  console.log(word);
 }
 
-reorderingLetter(“DARWINFOX”, 3) //DIXAWNORF
-reorderingLetter(“ABCDEFGHIJKLMNOPQ”, 5) //AIQBHJPCGKODFLNEM
+reorderingLetter("DARWINFOX", 3) //DIXAWNORF
+reorderingLetter("ABCDEFGHIJKLMNOPQ", 5) //AIQBHJPCGKODFLNEM
